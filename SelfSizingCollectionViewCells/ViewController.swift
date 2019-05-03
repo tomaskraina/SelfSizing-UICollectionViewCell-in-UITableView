5ea1dda4304867401aa8a85e593b3c1b7868e2a5
Tom Kraina <tomas.kraina@gmail.com>
2019-05-03 17:40:37 +0200

    Initial Commit

---
+++ SelfSizingCollectionViewCells/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SelfSizingCollectionViewCells
+//
+//  Created by Tom Kraina on 03/05/2019.
+//  Copyright © 2019 Tom Kraina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
